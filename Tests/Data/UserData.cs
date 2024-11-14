0b354516b61a35fb5be84278740f404d052df23f
r9n9ga7 <r9n9ga7@gmail.com>
2024-11-14 22:26:41 +0100

    [Test] [Account Controller] added tests for user creating

---
+++ Tests/Data/UserData.cs
@@ -0,0 +1,61 @@
+﻿
+using Server.Models.Requests;
+using System.Collections;
+
+namespace Tests.Data;
+
+public class UserData
+{
+    public static UserCreateRequest GetUserCreateRequest()
+    {
+        return new UserCreateRequest
+        {
+            Username = "username",
+            Password = "password",
+        };
+    }
+}
+
+public class UserDataInvalid : IEnumerable<object[]>
+{
+    public IEnumerator<object[]> GetEnumerator()
+    {
+        yield return new object[]
+        {
+            new UserCreateRequest
+            {
+                Username = "",
+                Password = "password",
+            }
+        };
+
+        yield return new object[]
+        {
+            new UserCreateRequest
+            {
+                Username = "username",
+                Password = "",
+            }
+        };
+
+        yield return new object[]
+        {
+            new UserCreateRequest
+            {
+                Username = new string('p', 128),
+                Password = "password",
+            }
+        };
+
+        yield return new object[]
+        {
+            new UserCreateRequest
+            {
+                Username = "username",
+                Password = new string('p', 128),
+            }
+        };
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
---
+++ Tests/Integration/AccountControllerTest.cs
@@ -0,0 +1,66 @@
+﻿using FluentAssertions;
+using Server.Interfaces.Repositories;
+using Server.Models.Requests;
+using System.Net;
+using Tests.Common;
+using Tests.Data;
+using Tests.Factories;
+
+namespace Tests.Integration;
+
+public class AccountControllerTest : IntegrationTestBase
+{
+    public AccountControllerTest(WebApplicationFactoryBase factory)
+        : base(factory)
+    {
+        _userRepository = GetService<IUserRepository>();
+    }
+
+    [Fact]
+    public async Task CreatingUserAccount()
+    {
+        var userCreateRequest = UserData.GetUserCreateRequest();
+
+        var response = await Post($"{Url}/create", userCreateRequest);
+        response.EnsureSuccessStatusCode();
+
+        var isUserExists = await _userRepository.Contains(userCreateRequest.Username);
+        isUserExists.Should().BeTrue();
+    }
+
+    [Fact]
+    public async Task UserPasswordIsHashed()
+    {
+        var userCreateRequest = UserData.GetUserCreateRequest();
+
+        var response = await Post($"{Url}/create", userCreateRequest);
+        response.EnsureSuccessStatusCode();
+
+        var user = await _userRepository.GetByUsername(userCreateRequest.Username);
+        user.Password.Should().NotBe(userCreateRequest.Password);
+    }
+
+    [Fact]
+    public async Task ShouldNotByAbleToCreateTwoUsersWithIdenticalUsernames()
+    {
+        var userCreateRequest = UserData.GetUserCreateRequest();
+
+        var firstUser = await Post($"{Url}/create", userCreateRequest);
+        firstUser.EnsureSuccessStatusCode();
+
+        var secondUser = await Post($"{Url}/create", userCreateRequest);
+        secondUser.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+    }
+
+    [Theory]
+    [ClassData(typeof(UserDataInvalid))]
+    public async Task CreatingUserWithInvalidData(UserCreateRequest userCreateRequest)
+    {
+        var secondUser = await Post($"{Url}/create", userCreateRequest);
+        secondUser.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+    }
+
+    const string Url = "/api/account";
+
+    readonly IUserRepository _userRepository;
+}
