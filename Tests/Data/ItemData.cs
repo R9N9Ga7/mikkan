b75360b76cc0410ab8ad11c311d8f3f53ef3953d
r9n9ga7 <r9n9ga7@gmail.com>
2024-11-27 22:55:42 +0100

    [Tests] added some tests for VaulController

---
+++ Tests/Data/ItemData.cs
@@ -0,0 +1,37 @@
+﻿using Server.Models.Requests;
+using System.Collections;
+
+namespace Tests.Data;
+
+public class ItemData
+{
+    public static AddItemRequest CreateAddItemRequest()
+    {
+        return new AddItemRequest
+        {
+            Login = $"Login-{Guid.NewGuid()}",
+            Password = $"Password-{Guid.NewGuid()}",
+            Name = $"Name-{Guid.NewGuid()}",
+        };
+    }
+}
+
+public class ItemDataInvalid : IEnumerable<object[]>
+{
+    public IEnumerator<object[]> GetEnumerator()
+    {
+        yield return new object[] {
+            new AddItemRequest { Login = "Login", Password = "Password", Name = "" }
+        };
+
+        yield return new object[] {
+            new AddItemRequest { Login = new string('l', 512), Password = "Password", Name = "Name" }
+        };
+
+        yield return new object[] {
+            new AddItemRequest { Login = "Login", Password = new string('l', 512), Name = "Name" }
+        };
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
---
+++ Tests/Integration/VaultControllerTest.cs
@@ -0,0 +1,55 @@
+﻿using FluentAssertions;
+using Server.Interfaces.Repositories;
+using Server.Models.Entities;
+using Server.Models.Requests;
+using System.Net;
+using Tests.Common;
+using Tests.Data;
+using Tests.Factories;
+
+namespace Tests.Integration;
+
+public class VaultControllerTest : IntegrationTestBase
+{
+    public VaultControllerTest(WebApplicationFactoryBase factory)
+        : base(factory)
+    {
+        _itemRepository = GetService<IItemRepository>();
+    }
+
+    [Fact]
+    public async Task AddItemWithValidData()
+    {
+        var itemRequest = ItemData.CreateAddItemRequest();
+
+        var response = await Post(UrlAddItem, itemRequest);
+        response.EnsureSuccessStatusCode();
+
+        var content = await DeserializeResponse<Item>(response);
+        content.Should().NotBeNull();
+
+        var isExists = await _itemRepository.IsExists(content.Id);
+        isExists.Should().BeTrue();
+
+        content.Should().BeEquivalentTo(itemRequest);
+
+        var findedItem = await _itemRepository.GetById(content.Id);
+        findedItem.Should().NotBeNull();
+
+        var user = await GetTestUser();
+        findedItem?.UserId.Should().Be(user.Id);
+    }
+
+    [Theory]
+    [ClassData(typeof(ItemDataInvalid))]
+    public async Task AddItemWithInvalidData(AddItemRequest itemRequest)
+    {
+        var response = await Post(UrlAddItem, itemRequest);
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+    }
+
+    const string Url = "api/vault";
+    const string UrlAddItem = Url;
+
+    readonly IItemRepository _itemRepository;
+}
