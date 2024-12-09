5e539cb7cf17e58fb6054905cd760e5ccd37c90b
r9n9ga7 <r9n9ga7@gmail.com>
2024-12-09 22:20:06 +0100

    [Client] added layouts

--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -1,11 +1,14 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import App from './app/App.tsx';
+
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+
+import router from './router.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 );
--- Client/src/router.tsx
+++ Client/src/router.tsx
@@ -1,11 +1,26 @@
 import { createBrowserRouter } from 'react-router-dom';
+
 import App from './app/App';
+
+import MainLayout from './layouts/MainLayout';
+import AuthLayout from './layouts/AuthLayout';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
-    children: [],
+    children: [
+      {
+        path: '/',
+        element: <MainLayout />,
+        children: [],
+      },
+      {
+        path: '/auth',
+        element: <AuthLayout />,
+        children: [],
+      },
+    ],
   },
 ]);
 
