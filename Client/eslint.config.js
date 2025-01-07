fe2eab6d11557bdcd15e6d67110f64c3f1878a27
r9n9ga7 <r9n9ga7@gmail.com>
2025-01-07 23:12:40 +0100

    [Client] added more strict rules for code validators

--- Client/eslint.config.js
+++ Client/eslint.config.js
@@ -28,6 +28,43 @@ export default tseslint.config(
       'comma-dangle': ['error', 'always-multiline'],
       'indent': ['error', 2],
       'jsx-quotes': ['error', 'prefer-double'],
+      'no-duplicate-imports': ['error'],
+
+      '@typescript-eslint/naming-convention': [
+        'error',
+        {
+          'selector': 'function',
+          'format': ['camelCase'],
+        },
+        {
+          'selector': 'method',
+          'format': ['camelCase'],
+        },
+      ],
+
+      '@typescript-eslint/no-empty-object-type': [
+        'error',
+        {
+          allowInterfaces: 'with-single-extends',
+        },
+      ],
+
+      '@typescript-eslint/no-explicit-any': 'error',
+
+      '@typescript-eslint/explicit-function-return-type': [
+        'error',
+        {
+          allowExpressions: false,
+          allowTypedFunctionExpressions: true,
+        },
+      ],
+
+      '@typescript-eslint/explicit-member-accessibility': [
+        'error',
+        {
+          accessibility: 'explicit',
+        },
+      ],
     },
   },
 );
