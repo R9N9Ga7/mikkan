4103bf84fe8e288748c646e134370f5761552d13
r9n9ga7 <r9n9ga7@gmail.com>
2025-01-15 22:48:47 +0100

    [Client] added logout

--- Client/src/layouts/main/Header.tsx
+++ Client/src/layouts/main/Header.tsx
@@ -1,9 +1,17 @@
 import { FC } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { Button, Container, Nav, Navbar } from 'react-bootstrap';
-import { ADD_ITEM_PARAM_FULL_URL, MAIN_PAGE_URL } from '../../common/consts/pages_urls';
+import { ADD_ITEM_PARAM_FULL_URL, LOGIN_FULL_URL, MAIN_PAGE_URL } from '../../common/consts/pages_urls';
+import AccountStorage from '../../utils/account_storage';
 
 const Header: FC = () => {
+  const navigate = useNavigate();
+
+  const handleOnLogout = (): void => {
+    AccountStorage.clear();
+    navigate(LOGIN_FULL_URL);
+  };
+
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container fluid>
@@ -13,7 +21,10 @@ const Header: FC = () => {
           <Nav className="me-auto">
             <Nav.Link as={Link} to={ADD_ITEM_PARAM_FULL_URL}>Add Item</Nav.Link>
           </Nav>
-          <Button variant="outline-danger">Logout</Button>
+          <Button
+            variant="outline-danger"
+            onClick={handleOnLogout}
+          >Logout</Button>
         </Navbar.Collapse>
       </Container>
     </Navbar>
